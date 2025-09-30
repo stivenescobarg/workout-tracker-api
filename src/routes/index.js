70f2d686046ec01d89d05b1e3d2cb8ddbea6dcd4
stivenescobarg <stivenescobar240208@gmail.com>
2025-09-30 12:25:21 -0500

    "Implementación del metodo get, en la rama de users"

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,3 +3,18 @@ const router = express.Router();
 
 // importar versiones de rutas
 const v1Routes = require('./v1');
+
+// Ruta base para información de la API
+router.get('/', (req, res) => {
+  res.json({ 
+    message: 'Workout Tracker API',
+    version: ['v1'],
+    endpoints: {
+        v1: '/api/v1'
+    }
+  });
+});
+
+router.use('/v1', v1Routes);
+
+module.exports = router;
--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+// Estado de memoria (simulación de base de datos)
+let users = [
+  {
+    id: "1",
+    nombre: "stiven Escobar",
+    email: "stivenescobar240208@gmail.com",
+    contraseña_hash: "123456SEG",
+    fecha_creacion: "2025-09-18",
+    fecha_actualizacion: "2025-09-18",
+    estatura_cm: 175,
+    peso_kg: 72,
+    fecha_nacimient:"2008-02-24",
+    role: "user",
+    createdAt: "2025-09-12T12:00:00Z"
+  }
+];
+
 // GET /api/v1/users
-router.get('/', (req, res) => {
-  res.send('GET all users');
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const user = users.find(u => u.id === id);
+
+  if (!user) {
+    return res.status(404).json({ error: 'Usuario no encontrado' });
+  }
+
+  res.status(200).json(user);
 });
 
 module.exports = router;
